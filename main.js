a07d8c1b81a9033452de72dcab32ab28a894ab61
Michael Trieb <mikeytrieb@gmail.com>
2017-03-13 13:42:25 -0500

    Wrote HTML, and created JS file with drawPyramid function written

---
+++ main.js
@@ -0,0 +1,65 @@
+
+
+// Figure out which symbol the user chose
+
+
+
+
+
+
+
+
+function drawPyramid() {
+
+
+    //  User Favored Character
+        var symbol =  $("#symbol").val();
+
+
+    //  Slider value, converted to integer
+        var heightStr   = $("#height").val();
+        var height      = parseInt(heightStr);
+
+
+    //  Clear the content area
+        $("#pyramid").empty();
+
+
+    //  Deliver Variable Content to Content Area
+        for (var row = 0; row < height; row++) {
+
+            //  Determine number of Bricks and Spaces
+                var numBricks = row + 2;
+                var numSpaces = height - row - 1;
+
+            //  Build string of Bricks, for each Row
+                var rowStr = "";
+                for (var i = 0; i < numSpaces; i++) {
+                    var spaceChar = "&nbsp";
+                    rowStr += spaceChar;
+                }
+                for (var i = 0; i < numBricks; i++) {
+                    rowStr += symbol;
+                    console.log("Symbol Value", rowStr);
+                }
+
+            //  Build Content Block, Append to Content Area
+                rowElem = $("<p>").html(rowStr);
+                $("#pyramid").append(rowElem);
+        }
+
+        console.log("It's getting here");
+}
+
+$(document).ready(function() {
+
+
+
+
+    $("#height").on("change", drawPyramid());
+
+
+
+
+
+});
